d91379860732d45e83faafc0b2314f6d5000f6e5
Basangtgrg <np01cp4s210269@islingtoncollege.edu.np>
2022-09-19 09:49:32 +0545

    first commit

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace year3consoleapp
+{
+    internal class FirstClass
+    {
+        public void First()
+        {
+            Console.WriteLine("Test class");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using year3consoleapp;
+
+FirstClass test = new FirstClass();
+
+test.First();
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
